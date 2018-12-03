f16c698ed84623df55d7b2030a5526d102e160da
Jorge De Pablo Martinez <jdepablo@gamma04.aulas.gsyc.urjc.es>
2018-12-03 10:57:53 +0100

    primeros pasos

---
+++ uaclient.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""UA Client UDP implement a socket to a register server."""
+
+import socket
+import sys
+
+# UA Client UDP simple.
+
+# Pick config, method and option of keyboard.
+try:
+    CONFIG = sys.argv[1]
+    METHOD = sys.argv[2]
+    OPTION = sys.argv[3]
+except (IndexError, ValueError):
+    sys.exit('Usage: aclient.py config method option')
+
+# Content to send
+LINE = METHOD + ' sip:' + LOGIN + '@' + SERVER + ' SIP/2.0\r\n'
+
+# Create the socket, configure it and attach it to server/port
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((SERVER, PORT))
+
+    print('Sending: ' + LINE)
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    data = my_socket.recv(1024)
+    response = data.decode('utf-8')
+    print(response)
+    if response.split()[1] == '100':
+        ack = 'ACK sip:' + LOGIN + '@' + SERVER + ' SIP/2.0\r\n'
+        my_socket.send(bytes(ack, 'utf-8') + b'\r\n')
+        print('Sending: ' + ack)
+    print('Ending socket...')
+
+print('Socket done.')
---
+++ uaserver.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Class (and main program) for echo register server in UDP simple."""
+
+import socketserver
+import sys
+import os
+
+try:
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
+    FICH = sys.argv[3]
+except (IndexError, ValueError):
+    sys.exit('Usage: python3 server.py IP port audio_file')
+
+TRYNING = b'SIP/2.0 100 Trying\r\n\r\n'
+RING = b'SIP/2.0 180 Ring\r\n\r\n'
+OK = b'SIP/2.0 200 OK\r\n\r\n'
+NOT_ALLOWED = b'SIP/2.0 405 Method Not Allowed\r\n\r\n'
+BAD_REQUEST = b'SIP/2.0 400 Bad Request\r\n\r\n'
+
+
+class EchoHandler(socketserver.DatagramRequestHandler):
+    """Echo server class."""
+
+    def check_request(self, mess):
+        """Check if the SIP request is correctly formed."""
+        self.correct = True
+        valid_characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                            'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+                            'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                            'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7',
+                            '8', '9', '_', '-', '.']
+        try:
+            body = mess.split()[1]
+            version = mess.split()[2]
+            user = body.split('@')[0]
+            ip = body.split('@')[1]
+        except IndexError:
+            self.correct = False
+
+        if len(mess.split()) != 3:
+            self.correct = False
+        if version != 'SIP/2.0':
+            self.correct = False
+        if user.split(':')[0] != 'sip':
+            self.correct = False
+        for character in user.split(':')[1]:
+            if character not in valid_characters:
+                self.correct = False
+
+        return self.correct
+
+    def handle(self):
+        """Handle method of the server class."""
+        received_mess = []
+        for index, line in enumerate(self.rfile):
+            received_mess = line.decode('utf-8')
+            if index == 0:
+                # Reading the first string that client send
+                client = received_mess.split(':')[1].split('@')[0]
+                print(client + ' send: ' + received_mess)
+                if self.check_request(received_mess):
+                    method = received_mess.split()[0]
+                    if method == 'INVITE':
+                        self.wfile.write(TRYNING)
+                        self.wfile.write(RING)
+                        self.wfile.write(OK)
+                        print('Sending 100 Trying')
+                        print('Sending 180 Ring')
+                        print('Sending 200 OK')
+                    elif method == 'BYE':
+                        self.wfile.write(OK)
+                        print('Sending 200 OK')
+                    elif method == 'ACK':
+                        ToRun = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + FICH
+                        print('Running: ', ToRun)
+                        os.system(ToRun)
+                    else:
+                        self.wfile.write(NOT_ALLOWED)
+                        print('Sending 405 Method Not Allowed')
+                else:
+                    self.wfile.write(BAD_REQUEST)
+                    print('Sending 400 Bad Request')
+            else:
+                # If no more lines, exit of the loop.
+                break
+
+
+if __name__ == "__main__":
+    """Create echo server and listening."""
+    serv = socketserver.UDPServer((SERVER, PORT), EchoHandler)
+    print('Listening...')
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print('  Server interrupt')
