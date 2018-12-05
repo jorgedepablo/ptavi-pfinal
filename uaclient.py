c630cd1ab73f25e4ca4dea1db818e812a12025e3
Jorge De Pablo Martinez <jdepablo@gamma04.aulas.gsyc.urjc.es>
2018-12-05 11:08:16 +0100

    cosas

--- uaclient.py
+++ uaclient.py
@@ -4,6 +4,8 @@
 
 import socket
 import sys
+import socketserver
+import time
 
 # UA Client UDP simple.
 
@@ -15,37 +17,53 @@ try:
 except (IndexError, ValueError):
     sys.exit('Usage: aclient.py config method option')
 
-def register():
-    print('lo del register')
+header = []
+
+def register(authentication):
+    REQUEST = 'REGISTER sip:' + LOGIN + ':' + PORT  + ' SIP/2.0\r\n\r\n')
+    header[0] = 'Expires: ' OPTION + '\r\n\r\n'
+    if authentication:
+        header[1] = 'Authorization: Digest response="' + PSW + '"\r\n\r\n'
+
+def invite():
+    print('lo del invite')
+
+def bye():
+    print('lo del bye')
 
 # Content to send
 try:
-    if METHOD = 'REGISTER':
-        register()
-    elif METHOD = 'INVITE':
-        print('lo del invite')
-    elif METHOD = 'BYE':
-        print('lo del bye')
+    if METHOD == 'REGISTER':
+        authentication = False
+        register(authentication)
+    elif METHOD == 'INVITE':
+        invite()
+    elif METHOD == 'BYE':
+        bye()
     else:
         exit('Usage: method not avaleible')
 except (IndexError, ValueError):
     exit('Usage: aclient.py config method option')
 
+def send_cosas: 
+    print('Sending: ' + REQUEST)
+    my_socket.send(bytes(REQUEST, 'utf-8'))
+    for item in range(len(header)):
+        print('Sending: ' + header[item])
+        my_socket.send(bytes(header[item], 'utf-8'))
 
 # Create the socket, configure it and attach it to server/port
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT))
 
-    print('Sending: ' + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    response = data.decode('utf-8')
-    print(response)
-    if response.split()[1] == '100':
-        ack = 'ACK sip:' + LOGIN + '@' + SERVER + ' SIP/2.0\r\n'
-        my_socket.send(bytes(ack, 'utf-8') + b'\r\n')
-        print('Sending: ' + ack)
+
+    DATA = my_socket.recv(1024)
+    RESPONSE = data.decode('utf-8')
+    print(RESPONSE)
+    if RESPONSE.split()[1] == '401':
+        authentication = True
+        register(authentication)
     print('Ending socket...')
 
 print('Socket done.')
